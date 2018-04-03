07e0fdcf5949c077448ba463d47fe58526ec2839
Felix Labelle <flx.labelle@gmail.com>
2018-04-03 11:56:11 -0400

    Fixed maskRcnn link and coco setup problem

--- segmentation/install_package.bash
+++ segmentation/install_package.bash
@@ -4,8 +4,9 @@ CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/devine_segmentation"
 echo "export ROS_PACKAGE_PATH=$CURDIR:\$ROS_PACKAGE_PATH" >> ~/.bashrc
 
 #Env setup
+sudo pip install --upgrade setuptools
 sudo pip3 install Cython scikit-image bson pymongo pycocotools keras catkin_pkg rospkg
-wget https://github.com/matterport/Mask_RCNN/releases/download/v2.1/mask_rcnn_coco.h5
+wget https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5
 
 #Reload env
 exec bash
