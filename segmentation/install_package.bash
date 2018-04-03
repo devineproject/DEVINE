2336fc7473866677c6a6abcaa275cd880cdc8ac3
Felix Labelle <flx.labelle@gmail.com>
2018-04-03 12:03:43 -0400

    Pip install correction

--- segmentation/install_package.bash
+++ segmentation/install_package.bash
@@ -4,7 +4,7 @@ CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/devine_segmentation"
 echo "export ROS_PACKAGE_PATH=$CURDIR:\$ROS_PACKAGE_PATH" >> ~/.bashrc
 
 #Env setup
-sudo pip install --upgrade setuptools
+sudo pip3 install --upgrade setuptools
 sudo pip3 install Cython scikit-image bson pymongo pycocotools keras catkin_pkg rospkg
 wget https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5
 
