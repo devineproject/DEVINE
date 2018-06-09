6271ea98b3e4b39f2078158574e62ad667c14b37
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2018-06-09 17:51:27 -0400

    Added routing to python web server

--- dashboard/serve.py
+++ dashboard/serve.py
@@ -10,10 +10,11 @@ aiohttp_jinja2.setup(
 )
 
 async def index(request):
-    return aiohttp_jinja2.render_template('index.html', request, {})
+    path = request.match_info.get('path', "index")
+    return aiohttp_jinja2.render_template('index.html', request, {'path': path})
 
 app.router.add_static('/assets', 'assets')
 app.router.add_static('/dist', 'dist')
 app.router.add_get('/', index)
-app.router.add_get('/{name}', index) # anything else is frontend routed
+app.router.add_get('/{path}', index) # anything else is frontend routed
 web.run_app(app)
