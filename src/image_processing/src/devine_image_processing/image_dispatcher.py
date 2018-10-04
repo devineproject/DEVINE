8a6bbf87d46f28e54337f738e2cd3797eb424f67
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2018-10-04 00:20:51 -0400

    Fixed queue import for both python2&3 in image_dispatcher.py

--- src/image_processing/src/devine_image_processing/image_dispatcher.py
+++ src/image_processing/src/devine_image_processing/image_dispatcher.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python2
 '''Image dispatching node'''
 
-from queue import Queue, Empty
+try:
+    from queue import Queue, Empty
+except:
+    from Queue import Queue, Empty
 import rospy
 from std_msgs.msg import Bool
 from sensor_msgs.msg import CompressedImage
