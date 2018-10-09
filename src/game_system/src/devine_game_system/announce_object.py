0dc83d2aaeca7befd374c9cf0e3f8c3c18b0f447
Felix Labelle <flx.labelle@gmail.com>
2018-10-09 17:09:13 -0400

    Bug fixes

--- src/game_system/src/devine_game_system/announce_object.py
+++ src/game_system/src/devine_game_system/announce_object.py
@@ -2,7 +2,7 @@
 ''' Announce selected object once IRL-1 has pointed to it'''
 
 import rospy
-from queue import Queue, Empty
+from Queue import Queue, Empty
 from std_msgs.msg import Bool
 from std_msgs.msg import String
 from devine_config import topicname
@@ -13,42 +13,40 @@ TOPIC_OBJECT_CATEGORY = topicname('guess_category')
 TOPIC_SNIPS = topicname('tts_answer')
 TOPIC_IS_POINTING_OBJECT = topicname('is_pointing_object') # add to paramters.json
 TOPIC_IS_END_OF_GAME = topicname('end_of_game') # add to parameters.json
-
+global current_category 
+global pointing_state
 def category_callback(data):
 	'''Callback for the category topic'''
-    if categories.full():
-        categories.get()
-
-    categories.put(data.data)
+	current_category = data.data
 	
 def is_pointing_callback(data):
 	'''Callback for the is_pointing topic'''
-    if is_pointing.full():
-        is_pointing.get()
+	print(data.data)
+	pointing_state = data.data
 
-    is_pointing.put(data.data)
-
-def main(args):
-    ''' Checks if selected object has been pointed to and announces its category '''
+def main():
+	''' Checks if selected object has been pointed to and announces its category '''
 	rospy.init_node(NODE_NAME)
-	global is_pointing = Queue(1)
-	global categories = Queue(1)
+	current_category = ""
+	pointing_state = False
 	rospy.Subscriber(TOPIC_OBJECT_CATEGORY, String, category_callback)
 	rospy.Subscriber(TOPIC_IS_POINTING_OBJECT, Bool, is_pointing_callback)
-    end_of_game = rospy.Publisher(TOPIC_OBJECT_CATEGORY, Bool, queue_size=1)
+	end_of_game = rospy.Publisher(TOPIC_IS_END_OF_GAME, Bool, queue_size=1)
 	snips = rospy.Publisher(TOPIC_SNIPS, String, queue_size=1)
-
-    rate = rospy.Rate(1) # TBD
-    while not rospy.is_shutdown():
-        if is_pointing.get() == True:
-            object_type = categories.get()
-			snips.publish(object_type)
+	print("Here")
+	rospy.sleep(1)
+	rate = rospy.Rate(1) # TBD
+	while not rospy.is_shutdown():
+		if pointing_state == True:
+			print("Cond met")
+			snips.publish(current_category)
 			rospy.sleep(1)
 			end_of_game.publish(True)
-
+		print("Loopy")
 		rate.sleep()
+	print("Loop exited")
 
 
 if __name__ == '__main__':
-    main()
+	main()
 	rospy.spin()
