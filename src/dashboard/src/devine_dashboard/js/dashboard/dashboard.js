944a1b11bd20e1e3cdfe01d9f7131ebff942a02d
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2018-10-09 18:22:56 -0400

    Removed the dashboard topic conf file

--- src/dashboard/src/devine_dashboard/js/dashboard/dashboard.js
+++ src/dashboard/src/devine_dashboard/js/dashboard/dashboard.js
@@ -1,16 +1,19 @@
+import $ from 'jquery';
 import publisher from './publisher';
 import gamestate from './subscribers/gamestate';
 import kinect from './subscribers/kinect';
 import dialog from './subscribers/dialog';
 import guesswhat from './subscribers/guesswhat';
 
-export default function CreateDashboard() {
+export default function CreateDashboard(topics) {
   $(document).ready(function () {
     // Uncheck all subscriptions
     $('[type="checkbox"]').prop('checked', false);
-  });
 
-  kinect();
-  dialog();
-  // TODO: IMPLEMENT OTHER MODULE CONSTRUCTORS
+    publisher(topics);
+    kinect(topics);
+    dialog(topics);
+    gamestate(topics);
+    guesswhat(topics);
+  });
 }
