c5a790cf813cec4188f8214534f394753973a7c7
Felix Labelle <flx.labelle@gmail.com>
2018-10-09 10:37:15 -0400

    Made changes + updated list_diff

--- tests/image_tests/image_segmentation.py
+++ tests/image_tests/image_segmentation.py
@@ -1,14 +1,11 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2
 ''' Test to validate segmentation rates '''
-import copy
 import argparse
 import rospy
-from std_msgs.msg import Float32MultiArray
 from devine_config import topicname
 from Queue import Queue, Empty
 import json
 import cv2
-import os
 from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
@@ -19,18 +16,32 @@ SEGMENTATION_IMAGE_TOPIC = topicname('objects')
 
 def list_diff(x1,x2):
     """ Returns the difference between two lists"""
-    return [[x for x in x1 if x not in x2],[x for x in x2 if x not in x1]]
+    i = len(x1)-1
+    j = len(x2)-1
+    x1.sort()
+    x2.sort()
+    while i >= 0 and j >= 0:
+    	if x1[i] > x2[j]:
+    		i-=1
+    	elif x1[i] < x2[j]:
+    		j-=1
+    	else:
+    		del x1[i]
+    		del x2[j]
+    		i-=1
+    		j-=1
+    return [x1,x2]
     
 def load_test_data(test_name):
-    """ Loads test data and images"""	
+    """ Loads test data and images"""    
     imgs = []
     data = []
     with open(test_name + '.json') as json_data:
-    	test_data = json.load(json_data)
+        test_data = json.load(json_data)
 
     for test in test_data["tests"]:
-    	imgs.append(cv2.imread(test["imageName"],cv2.IMREAD_COLOR))
-    	data.append(test["refData"]["objects"])
+        imgs.append(cv2.imread(test["imageName"],cv2.IMREAD_COLOR))
+        data.append(test["refData"]["objects"])
     return imgs,data
 
 def flatten_json(data):
@@ -40,11 +51,11 @@ def flatten_json(data):
 def segmentation_call_back(data):
     '''Callback for segmentation topic'''
     if seg_queue.full():
-    	seg_queue.get()
+        seg_queue.get()
     try:
-    	seg_queue.put(flatten_json(json.loads(data.data)))
+        seg_queue.put(flatten_json(json.loads(data.data)))
     except Exception as e:
-    	rospy.logerr(e)
+        rospy.logerr(e)
 
 def run_test(image,ref_data):
     """ Evaluates  segmentation rate for a single image """
@@ -52,7 +63,7 @@ def run_test(image,ref_data):
     segmentation_pub = rospy.Publisher(IMAGE_TOPIC, CompressedImage, queue_size=1)
     rospy.Subscriber(SEGMENTATION_IMAGE_TOPIC, String, segmentation_call_back)
     #is_blurry = is_image_blurry(image.data)
-    image_message = CvBridge().cv2_to_imgmsg(image)	
+    image_message = CvBridge().cv2_to_imgmsg(image)    
     ### Create CompressedIamge ####
     msg = CompressedImage()
     msg.header.stamp = rospy.Time.now()
@@ -61,22 +72,21 @@ def run_test(image,ref_data):
     rospy.sleep(1)
     segmentation_pub.publish(msg)
     rospy.sleep(1)
-    #recieve data
+    #receive data
     try:
-    	results = seg_queue.get()
-    	[missed_detections,false_detections] = list_diff(ref_data,results)
-    	missed_detection_count = len(missed_detections)
-    	false_detection_count = len(false_detections)
+        results = seg_queue.get()
+        [missed_detections,false_detections] = list_diff(ref_data,results)
+        missed_detection_count = len(missed_detections)
+        false_detection_count = len(false_detections)
     except Empty:
-    	missed_detection_count = -1
-    	false_detection_count = -1
-    	pass
+        missed_detection_count = -1
+        false_detection_count = -1
     #extract and comp dat
     return [missed_detection_count,false_detection_count]
     
 
 def main(args):
-    ''' Publish on the 3D position from /base_link to point '''
+    '''Loads images and posts the corresponding segmentation rates'''
     # Parse arguments
     test_name = args.test
     # Start ROS node
@@ -87,13 +97,13 @@ def main(args):
     pos = 0
     rate = rospy.Rate(1)
     while not rospy.is_shutdown() and pos < len(imgs):
-    	[missed_detection_count,false_detection_count] = run_test(imgs[pos],data[pos])
-    	pos += 1
-    	rospy.loginfo("Num missed detections" + str(missed_detection_count))
-    	rospy.loginfo("Num false detections: " + str(false_detection_count))
-    	rate.sleep()
-    	print("Percentage of objects missed: " + str(float(missed_detection_count)/len(data[pos-1])))
-    	print("Number of objects falsely detected: ",false_detection_count)
+        [missed_detection_count,false_detection_count] = run_test(imgs[pos],data[pos])
+        pos += 1
+        rospy.loginfo("Num missed detections" + str(missed_detection_count))
+        rospy.loginfo("Num false detections: " + str(false_detection_count))
+        rate.sleep()
+        print("Percentage of objects missed: " + str(float(missed_detection_count)/len(data[pos-1])))
+        print("Number of objects falsely detected: ",false_detection_count)
 
 def parser():
     ''' Command Line Parser'''
