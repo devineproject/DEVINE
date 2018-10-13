0241857667961e88eefd2e6cbe96aeed2eb54e87
FelixLabelle <flx.labelle@gmail.com>
2018-10-13 18:27:23 -0400

    Set correct package in setup.py

--- src/game_system/setup.py
+++ src/game_system/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup()
-setup_args['packages'] = ['devine_common']
+setup_args['packages'] = ['devine_game_system']
 setup_args['package_dir'] = {'': 'src'}
 
 setup(**setup_args)
