6a3766c9013cae9ac70633c82780c9b45130d7d1
Felix Labelle <flx.labelle@gmail.com>
2018-10-21 14:40:26 -0400

    Refactored mscoco test code

--- tests/pipeline_testing/play_mscoco.py
+++ tests/pipeline_testing/play_mscoco.py
@@ -1,4 +1,6 @@
-t to validate segmentation rates '''
+#! /usr/bin/env python2
+'''import mscoco images to validate pipeline'''
+
 import argparse
 import random
 import rospy
@@ -13,82 +15,59 @@ from std_msgs.msg import String
 from std_msgs.msg import Bool
 from devine_dialog.msg import TtsQuery
 from devine_dialog import TTSAnswerType, send_speech
-
+from time import sleep
+import os
 # How to load images from mscoco
 
-IMAGE_TOPIC = topicname('raw_image')
+FEATURE_IMAGE_TOPIC = topicname('features_extraction_image')
+SEGMENTATION_IMAGE_TOPIC = topicname('segmentation_image')
 RESTART_TOPIC = topicname('end_of_game')
-GUESSED_OBJECT_TOPIC = topic('guess_category')
 TTS_ANSWER_TOPIC = topicname('tts_answer')
 TTS_QUERY_TOPIC = topicname('tts_query')
 CATEGORY_TOPIC = topicname('guess_category')
 
 class MSCOCOTest:
-    def __init__(self,image_folder,num_games):
-        self.images = []
+    ''' Test class that loads images and runs a game of GW?! '''
+    def __init__(self,image_folder):
         self.image_folder = image_folder
-        self.num_games = num_games
-        self._load_test_data()
-		self.question = ""
-        self.guessed_category = ""
-        
+        self.image = None 
         rospy.init_node('mscoco_test')
         self.rate = rospy.Rate(1)
-        self.image_pub = rospy.Publisher(IMAGE_TOPIC, CompressedImage, queue_size=1)
-        self.restart_pub = rospy.Publisher(RESTART_TOPIC, Bool, queue_size=1)
-		self.answer_pub = rospy.Publisher(TTS_ANSWER_TOPIC, TtsQuery, queue_size=1)
-		rospy.Subscriber(CATEGORY_TOPIC, String, self.category_callback)
-		rospy.Subscriber(TTS_QUERY_TOPIC, TtsQuery, self.recieve_question)
-        rospy.Subscriber(GUESSED_OBJECT_TOPIC, String, self.guessed_category_callback) 
-        rospy.sleep(1)
+        self.image_feat_pub = rospy.Publisher(FEATURE_IMAGE_TOPIC, CompressedImage, queue_size=1)
+        self.image_seg_pub = rospy.Publisher(SEGMENTATION_IMAGE_TOPIC, CompressedImage, queue_size=1)
+        sleep(1)
         
-    def category_callback(self,category):
-		self.guessed_category = category.data
+    def _load_test_image(self, image_name):
+        """ Loads test data and images """
+        if image_name is None:
+            image_list = os.listdir(self.image_folder)
+            random.shuffle(image_list)
+            image_name = image_list[0]
+        image_path = os.path.join(self.image_folder,image_name)
+        self.image = cv2.imread(image_path,cv2.IMREAD_COLOR)
 
-    def _load_test_data(self):
-        """ Loads test data and images"""
-        image_pathes = [self.image_folder + img for img in random.shuffle(os.listdir(self.image_folder))[0:self.num_games]]
-        for image_path in image_pathes: 
-            self.images.append(cv2.imread(image_path,cv2.IMREAD_COLOR))
-
-    def guessed_category_callback(self,guess):
-        '''Callback for segmentation topic'''
-        self.guessed_category = guess.data
-    
-    def _send_compressed_image(self,img):
+    def _send_compressed_image(self):
+        ''' Sends compressed image to feature and seg nodes '''
         msg = CompressedImage()
         msg.header.stamp = rospy.Time.now()
         msg.format = "png"
-        msg.data = np.array(cv2.imencode('.png', img)[1]).tostring()
-        self.image_pub.publish(msg)
-		
-	def _send_answer(self):
-		answer_to_question = raw_input(self.question + ": ")
-		tts_query_object = {text: answer_to_question, uid: 1, answer_type: 0}
-		self.answer_pub.pub(json.dumps(data))
-	
-	def recieve_question(self, question):
-		self.question = question.data
-    
-    def play_game(self,img):
-        self._send_compressed_image(self,img)
-		for i in range(0,5):
-			self._send_answer()
-			self.rate.sleep()
-		return self.guessed_category
-		
-    def play_games(self)
-        return [self.play_game(self.images[i]) for i in range(0,self.num_games)]
-    
-
-
-    
-
+        msg.data = np.array(cv2.imencode('.png', self.image)[1]).tostring()
+        self.image_seg_pub.publish(msg) 
+        self.image_feat_pub.publish(msg)
+        
+    def play_game(self,image_name = None):
+        ''' Plays a game of GW?! '''
+        self._load_test_image(image_name)
+        self._send_compressed_image()
+        guess = rospy.wait_for_message(CATEGORY_TOPIC, String)
+        return guess
+        
 def main(args):
-    '''Loads images and posts the corresponding segmentation rates'''
+    ''' Plays n games and their outputs  '''
     # Parse arguments
-    test = MSCOCOTest(args.dataset,args.num)
-    print(test.play_games())
+    test = MSCOCOTest(args.dataset)
+    for i in range(0,int(args.num)):
+         print(test.play_game())
 
  
 def parser():
@@ -103,4 +82,3 @@ def parser():
 
 if __name__ == '__main__':
     main(parser())
-    rospy.spin()
