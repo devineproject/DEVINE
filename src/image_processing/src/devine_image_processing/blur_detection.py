09b7f455653cc278f9f5c46ce828abd4c4e4edcd
Felix Labelle <flx.labelle@gmail.com>
2018-10-22 11:42:21 -0400

    changed file structre + changed conversion type

--- src/image_processing/src/devine_image_processing/blur_detection.py
+++ src/image_processing/src/devine_image_processing/blur_detection.py
@@ -6,6 +6,6 @@ DETECTION_THRESHOLD = 20
 
 def is_image_blurry(img):
     """ compute the variance of the Laplacian to measure focus"""
-    gray = cv2.cvtColor(cv2.cvtColor(img,cv2.COLOR_RGB2BGR), cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
     focus_measure = cv2.Laplacian(gray, cv2.CV_64F).var() 
     return focus_measure < DETECTION_THRESHOLD
