2935333ae5aeffd683a6a623b397dad926bf651a
Felix Labelle <flx.labelle@gmail.com>
2018-11-01 09:28:29 -0400

    Made modifications for PR

--- src/image_processing/src/devine_image_processing/mask.py
+++ src/image_processing/src/devine_image_processing/mask.py
@@ -15,30 +15,30 @@ IMAGE_PUB_TOPIC =  topicname('segmentation_image') #"/devine/image/depth_masked"
 DEPTH_THRESHOLD = 1.5 # Depth in meters
 
 class DepthMask(object):
-    ''' Node that applies a depth mask to RGB image and resends the masked image '''
+    """ Node that applies a depth mask to RGB image and resends the masked image """
     def __init__(self):
         self.bridge = CvBridge()
-        image_sub = message_filters.Subscriber("camera/rgb/image_color", ROS_Image)
-        depth_sub = message_filters.Subscriber("camera/depth/image", ROS_Image)
+        image_sub = message_filters.Subscriber('camera/rgb/image_color', ROS_Image)
+        depth_sub = message_filters.Subscriber('camera/depth/image', ROS_Image)
 
-        self.ts = message_filters.ApproximateTimeSynchronizer([image_sub, depth_sub], 10, 0.5) # check these params
+        self.ts = message_filters.ApproximateTimeSynchronizer([image_sub, depth_sub], 1, 0.5)
         self.ts.registerCallback(self.callback)
-        self.depth_mask_publisher = rospy.Publisher(IMAGE_PUB_TOPIC, CompressedImage, queue_size=10)
+        self.depth_mask_publisher = rospy.Publisher(IMAGE_PUB_TOPIC, CompressedImage, queue_size=1)
        
     def callback(self, rgb_data, depth_data):
         """ Call back to synchronised topics that applies the depth mask"""
         try:
-            image = self.bridge.imgmsg_to_cv2(rgb_data, "bgr8")
-            depth_image = self.bridge.imgmsg_to_cv2(depth_data, "passthrough")
+            image = self.bridge.imgmsg_to_cv2(rgb_data, 'bgr8')
+            depth_image = self.bridge.imgmsg_to_cv2(depth_data, 'passthrough')
         except CvBridgeError, e:
-            print e
+            rospy.logerr(e)
         nan_indices = np.isnan(depth_image)
         idx = np.where(~nan_indices,np.arange(nan_indices.shape[1]),0)
         np.maximum.accumulate(idx,axis=1, out=idx)
         filled_in_depth_image = depth_image[np.arange(idx.shape[0])[:,None], idx] 
         mask = filled_in_depth_image < DEPTH_THRESHOLD
-        self.masked_image = np.array([pixel if mask_val ==  True else np.array([0,0,0]) for pixel_row,bool_row in zip(image,mask) for pixel,mask_val in zip(pixel_row,bool_row)])
-        self.masked_image = np.reshape(self.masked_image,image.shape)
+        self.masked_image = image
+        self.masked_image[~mask] = [0, 0, 0]
         msg = image_to_ros_msg(self.masked_image)
         self.depth_mask_publisher.publish(msg)
                         
