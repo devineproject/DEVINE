409e8e1d94c497cd7ad912df9f0657b2c1519ac7
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2018-11-06 17:19:50 -0500

    Started the integration of human_finder in head_coordinator

--- src/dialog/src/devine_dialog/dialog_control.py
+++ src/dialog/src/devine_dialog/dialog_control.py
@@ -10,6 +10,7 @@ from devine_dialog.msg import TtsQuery
 from devine_dialog import TTSAnswerType, send_speech
 from devine_config import topicname
 from devine_common import ros_utils
+from devine_head_coordinator.look import Look
 
 # IN topics
 HUMAN_READY_DETECTED_TOPIC = topicname('body_tracking')
@@ -33,14 +34,15 @@ class DialogControl():
         dialogs_file = open(CONST_FILE)
         self.dialogs = json.loads(dialogs_file.read())
         self.can_start = True
+        self._look = Look()
 
     def send_sentence(self, sentence, answer_type, **format_args):
         """ Wrapper to randomly send a sentence from an array with send_speech """
         return send_speech(TTS_PUBLISHER, random.choice(self.dialogs[sentence]).format(**format_args), answer_type)
 
     def wait_for_player(self, message_name, **format_args):
-        """ Ask 5 time the human a question. Return true if he did answer 'yes' one time. """
-        for _i in range(2):
+        """ Ask 2 time the human a question. Return true if he did answer 'yes' one time. """
+        for _ in range(2):
             answer = self.send_sentence(message_name, TTSAnswerType.YES_NO, **format_args)
             if answer == 'yes' or not answer:
                 break
@@ -52,13 +54,10 @@ class DialogControl():
         """ Exception type for interruptions of the dialog """
         pass
 
-    def on_human_detected(self, human_object):
+    def loop(self):
         """ When a human is detected, begin the robot/human dialog """
-        humans = json.loads(human_object.data)
-        if not humans or not self.can_start:  # No humans detected
-            return
-        self.can_start = False
         try:
+            self._look.at_human() # Blocks until a human is found
             self.send_sentence('welcome', TTSAnswerType.NO_ANSWER)
             answer = self.send_sentence('ask_to_play', TTSAnswerType.YES_NO)
             if not answer == 'yes':
@@ -69,11 +68,15 @@ class DialogControl():
             self.send_sentence('instructions', TTSAnswerType.NO_ANSWER)
             if not self.wait_for_player('ready', player_name=player_name):
                 raise DialogControl.HumanDialogInterrupted()
+
+            self._look.at_scene()
             READY_TO_PLAY_PUBLISHER.publish(player_name)
 
-            object_cat_name = rospy.wait_for_message(OBJECT_CATEGORY_TOPIC, String).data
+            object_found = rospy.wait_for_message(OBJECT_CATEGORY_TOPIC, String).data
+
             rospy.wait_for_message(IS_POINTING_OBJECT_TOPIC, Bool)
-            answer = self.send_sentence('ask_got_it_right', TTSAnswerType.YES_NO, object_name=object_cat_name)
+
+            answer = self.send_sentence('ask_got_it_right', TTSAnswerType.YES_NO, object_name=object_found)
 
             GUESS_SUCCEEDED.publish(answer == 'yes')
 
@@ -94,8 +97,6 @@ class DialogControl():
 def hook_listeners():
     """ Create the dialogControl object and hook the listeners """
     dialog_control = DialogControl()
-    rospy.Subscriber(HUMAN_READY_DETECTED_TOPIC, String, dialog_control.on_human_detected)
-
 
 if __name__ == '__main__':
     rospy.init_node('dialog_control')
--- src/head_coordinator/include/devine_head_coordinator/look.py
+++ src/head_coordinator/include/devine_head_coordinator/look.py
@@ -2,26 +2,37 @@
 
 import rospy
 import actionlib
+from devine_config import topicname
 from devine_head_coordinator.msg import LookAtHumanAction, LookAtHumanGoal
+from std_msgs.msg import Bool
+
+SCENE_DETECTION_TOPIC = topicname('start_scene_detection')
+TOPIC_SCENE_FOUND = topicname('scene_found')
 
 class Look(object):
     """ Easy to use intent to movement translator """
+
     def __init__(self):
-        self.human_finder_client = actionlib.SimpleActionClient('devine_human_finder', LookAtHumanAction)
-        self.human_finder_client.wait_for_server()
+        self._human_finder_client = actionlib.SimpleActionClient(
+            'devine_human_finder', LookAtHumanAction)
+        self._human_finder_client.wait_for_server()
         self.humans_count = 0
+        self._start_scene_publisher = rospy.Publisher(SCENE_DETECTION_TOPIC, Bool, queue_size=1)
 
     def at_human(self):
         """ Block until a human is found, and then look at him/her """
-        self.humans_count = 0 #Reset previous human count
+        # TODO: Cancel look_at_scene_goal
+        self.humans_count = 0  # Reset previous human count
         goal = LookAtHumanGoal(period=rospy.rostime.Duration(0))
-        self.human_finder_client.send_goal(goal, feedback_cb=self._human_feedback_callback)
+        self._human_finder_client.send_goal(
+            goal, feedback_cb=self._human_feedback_callback)
         self._wait_for_human()
 
-    def at_scene(self, time=0):
+    def at_scene(self):
         """ Block until a scene is found, and then look at it """
-        self.human_finder_client.cancel_all_goals()
-        # TODO look and wait for scene
+        self._human_finder_client.cancel_all_goals()
+        self._start_scene_publisher.publish(True)
+        rospy.wait_for_message(TOPIC_SCENE_FOUND, Bool, timeout=None)
 
     def _human_feedback_callback(self, feedback):
         """ Callback method to update the human_count from the feedback CB """
