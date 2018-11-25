ad162ce0a8bd0df02136e377434d7b3406f2fa85
Julien Chouinard-Beaupré <16941940+jcBeaupre@users.noreply.github.com>
2018-11-25 16:13:56 -0500

    Added Talking expression (Lip Sync) (#177)
    * Publishes random talking emotion using surprised mouth emotion and pulses.

--- src/robot_control/src/devine_irl_control/node/node_facial_expression.py
+++ src/robot_control/src/devine_irl_control/node/node_facial_expression.py
@@ -12,68 +12,96 @@ __email__ = "devine.gegi-request@listes.usherbrooke.ca"
 __status__ = "Production"
 
 from enum import Enum
+import random
 import rospy
 from std_msgs.msg import Bool
 from std_msgs.msg import Float64MultiArray
-from jn0_face_msgs.msg import EmoIntensity
+from jn0_face_msgs.msg import EmoIntensity, EmoPulse
+from devine_dialog.msg import TtsQuery
 from devine_config import topicname
 
-
-class RobotExpression(Enum):
-    """ Valid expressions """
+class RobotFacialExpression(Enum):
+    """ Valid facial expressions """
 
     SURPRISE = 'Surprise'
+    SUPRISE_MOUTH = 'Surprise-mouth'
     SAD = 'Sad'
     ANGER = 'Anger'
     JOY = 'Joy'
 
-
 OBJECT_CONFIDENCE_TOPIC = topicname('objects_confidence')
 GAME_SUCCESS_TOPIC = topicname('object_guess_success')
-ROBOT_EXPRESSION_TOPIC = topicname('robot_facial_expression')
+ROBOT_TALKING_EXPRESSION_TOPIC = topicname('robot_talking_expression')
+ROBOT_FACIAL_EXPRESSION_TOPIC = topicname('robot_facial_expression')
 FACIAL_EXPRESSION_COMPLETED = topicname('robot_facial_expression_completed')
+TTS_QUERY_TOPIC = topicname('tts_query')
 
 NODE_NAME = 'facial_expression'
-
+TALKING_EXPRESSION_DURATION = 0.2
 
 class FacialExpression(object):
-    """ Subscribes to object confidence and publishes facial expression for a specific duration """
+    """ Subscribes to object confidence and publishes facial expressions for a specific duration """
 
-    EXPRESSION_DURATION = 10
+    FACIAL_EXPRESSION_DURATION = 10
 
     def __init__(self):
         self.showing_emotion = False
         self.confidence = None
 
-        self.robot_expression_publisher = rospy.Publisher(ROBOT_EXPRESSION_TOPIC,
+        self.robot_expression_publisher = rospy.Publisher(ROBOT_FACIAL_EXPRESSION_TOPIC,
                                                           EmoIntensity, queue_size=1)
+
         self.expression_completed_pub = rospy.Publisher(FACIAL_EXPRESSION_COMPLETED,
                                                         Bool, queue_size=1)
+
+        self.robot_talking_expression_publisher = rospy.Publisher(ROBOT_TALKING_EXPRESSION_TOPIC,
+                                                                  EmoPulse, queue_size=1)
+
+        rospy.Subscriber(TTS_QUERY_TOPIC, TtsQuery, self.on_tts_query)
+
         rospy.Subscriber(OBJECT_CONFIDENCE_TOPIC, Float64MultiArray,
-                         self.on_new_object_confidence)
+                         self.on_object_confidence)
 
         rospy.Subscriber(GAME_SUCCESS_TOPIC, Bool,
                          self.on_game_success)
 
-    def on_new_object_confidence(self, objects_confidence):
+    def on_tts_query(self, query):
+        """ Callback on new tts query. Shows talking expression to robot """
+        total_duration = round(2*len(query.text)/3) # TODO find a better way
+        self.show_talking_expression_for(total_duration)
+
+    def show_talking_expression_for(self, duration):
+        """ Sends the suprise-mouth emotion with a random intensity """
+        self.showing_emotion = True
+        while duration > 0:
+            self.robot_talking_expression_publisher.publish(
+                emo_type=RobotFacialExpression.SUPRISE_MOUTH.value,
+                intensity=random.random(),
+                duration=rospy.Duration.from_sec(TALKING_EXPRESSION_DURATION)
+                )
+            rospy.sleep(TALKING_EXPRESSION_DURATION/2)
+            duration -= 1
+
+        self.showing_emotion = False
+
+    def on_object_confidence(self, objects_confidence):
         """ Callback on new object confidence. Updates max confidence """
         self.confidence = max(objects_confidence.data)
 
     def on_game_success(self, game_success):
         """ Callback on end game. Shows emotion depending on success and confidence """
-        expression = self.get_expression(self.confidence, game_success.data)
-
+        expression = self.get_facial_expression(self.confidence, game_success.data)
         if expression is not None and not self.showing_emotion:
-            self.show_expression_for(expression, self.EXPRESSION_DURATION)
+            self.show_facial_expression_for(expression, self.FACIAL_EXPRESSION_DURATION)
 
-    def show_expression_for(self, expression, duration):
+    def show_facial_expression_for(self, expression, duration):
         """ Publishes the expression for a specific duration"""
         value = 1  # default value
         self.showing_emotion = True
 
         # Special case for anger which is a bit too intense
-        if expression == RobotExpression.ANGER:
-            value = 0.5
+        if expression == RobotFacialExpression.ANGER:
+            value = 0.7
 
         face_expression = EmoIntensity(name=expression.value, value=value)
         self.robot_expression_publisher.publish(face_expression)
@@ -89,31 +117,30 @@ class FacialExpression(object):
         rospy.sleep(0.5)
         self.expression_completed_pub.publish(True)
 
-    def get_expression(self, confidence, success):
+    def get_facial_expression(self, confidence, success):
         """ Get the wanted expression depending on the received confidence.
         Defaults to ANGER
         """
         expression = None
 
         if self.confidence is not None:
-            expression = RobotExpression.ANGER
+            expression = RobotFacialExpression.ANGER
 
             # Devine Won
             if success:
                 if 0 <= confidence < 0.6:
-                    expression = RobotExpression.SURPRISE
+                    expression = RobotFacialExpression.SURPRISE
                 else:
-                    expression = RobotExpression.JOY
+                    expression = RobotFacialExpression.JOY
 
             # Devine Lost
             else:
                 if 0 <= confidence < 0.6:
-                    expression = RobotExpression.SAD
+                    expression = RobotFacialExpression.SAD
                 else:
-                    expression = RobotExpression.ANGER
+                    expression = RobotFacialExpression.ANGER
 
         return expression
-
 
 if __name__ == '__main__':
     rospy.init_node(NODE_NAME)
